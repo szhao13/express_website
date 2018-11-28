74bfaabca15e6a381030ae42d94262347827080a
Shelley Zhao <shelleyzhao@Shelleys-MacBook-Pro.local>
2018-11-27 23:31:46 -0500

    added some dependencies

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var app = express();
 
 // add logging and static middleware to express
