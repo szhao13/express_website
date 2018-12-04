b5c9ba0ef26305409afd89ae3f25f88e2de69ece
Shelley Zhao <shelleyzhao3@gmail.com>
2018-12-04 13:44:39 -0500

    sample html for skillshare

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+// app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use('/', routes);
